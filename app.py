27f65ac3c3804b157190e801ed7d69317105d51d
Chris Del <drumgod101@gmail.com>
2019-02-08 18:57:22 -0500

    add all tmpltes. Links working- had to disable some JS

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask import render_template
+from flask import g
 
 app = Flask(__name__)
 app.config['TESTING'] = True
 app.config['ENV'] = 'development'
 app.config.from_pyfile("flask.cfg")
-print(f"{app.config}")
 
 
 # # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
-def hello_world():
+def render_index():
     return render_template('index.html')
+
+
+@app.route('/services')
+def render_services():
+    return render_template('services.html')
+
+@app.route('/about')
+def render_about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def render_contact():
+    return render_template('contact.html')
