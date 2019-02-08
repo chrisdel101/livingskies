d3f831caa358fe2bc22321e0d7d72ce8a325da01
Chris Del <drumgod101@gmail.com>
2019-02-08 16:57:17 -0500

    add new template. About to delete old

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.config['TESTING'] = True
+app.config['ENV'] = 'development'
+app.config.from_pyfile("flask.cfg")
+print(f"{app.config}")
+
+
+# # Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def hello_world():
