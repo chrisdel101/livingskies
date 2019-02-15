ce076d441d53fa09803fb48bd03665ccb9b73db0
Chris Del <drumgod101@gmail.com>
2019-02-15 00:28:27 -0500

    get form working

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import request
 from flask import render_template
 from flask import g
+from flask_mail import Mail
+
 import os
 
 app = Flask(__name__)
+mail = Mail(app)
+
 
 app.config.from_pyfile("settings.py")
 
@@ -24,6 +29,20 @@ def render_services():
 def render_about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET','POST' ])
 def render_contact():
-    return render_template('contact.html')
+    if(request.method == 'GET'):
+        print('Hello')
+        return render_template('contact.html')
+    elif(request.method == 'POST'):
+        # if(not request.form.name or not request.form.email or not request.form.message):
+        #     # TODO
+        #     print('missing info')
+
+        # if:
+        name = request.form[0][0]
+        email = request.form[1[0]]
+        message = request.form[2][0]
+        print(f"{name, email,message}")
+        # print(request.form[])
+        return render_template('contact.html')
