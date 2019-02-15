6a0808f83f0f37cc5432fbccd7dddb2e221317d8
Chris Del <drumgod101@gmail.com>
2019-02-15 14:22:15 -0500

    form works

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ from flask import request
 from flask import render_template
 from flask import g
 from flask_mail import Mail
+from flask_mail import Message
 
 import os
 
 app = Flask(__name__)
+# app.config.from_pyfile("settings.py")
+app.config.from_pyfile("flask.cfg")
+app.config.update(
+    MAIL_SERVER=app.config['MAIL_SERVER'],
+    MAIL_PORT=app.config['MAIL_PORT'],
+    MAIL_USE_SSL=app.config['MAIL_USE_SSL'],
+    MAIL_USERNAME=app.config['MAIL_USERNAME'],
+    MAIL_PASSWORD = app.config['MAIL_PASSWORD']
+)
+
 mail = Mail(app)
-
-
-app.config.from_pyfile("settings.py")
 
 # # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -32,17 +40,27 @@ def render_about():
 @app.route('/contact', methods=['GET','POST' ])
 def render_contact():
     if(request.method == 'GET'):
-        print('Hello')
+        print(app.config['MAIL_PASSWORD'])
+        print(app.config['MAIL_SERVER'])
+        print(app.config['MAIL_USERNAME'])
+        print(app.config['MAIL_PORT'])
         return render_template('contact.html')
     elif(request.method == 'POST'):
-        # if(not request.form.name or not request.form.email or not request.form.message):
-        #     # TODO
-        #     print('missing info')
+        if(not request.form.get('name') or not request.form.get('email') or not request.form.get('message')):
+        # TODO
+            print(request.form)
+            return
+        else:
+            print(app.config['MAIL_PASSWORD'])
+            print(app.config['MAIL_SERVER'])
+            print(app.config['MAIL_USERNAME'])
+            print(app.config['MAIL_PORT'])
+            name = request.form.get('name')
+            email = request.form.get('email')
+            message = request.form.get('message')
 
-        # if:
-        name = request.form[0][0]
-        email = request.form[1[0]]
-        message = request.form[2][0]
-        print(f"{name, email,message}")
-        # print(request.form[])
-        return render_template('contact.html')
+            msg = Message("Hello",
+                  sender=app.config['MAIL_USERNAME'],
+                  recipients=["arssonist@yahoo.com"])
+            mail.send(msg)
+            return render_template('contact.html')
